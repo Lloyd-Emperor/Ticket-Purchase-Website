4045f75685ac15905320349323154160ee8e31a7
Lloyd-Emperor <honourakhigbe6@gmail.com>
2025-01-08 01:56:24 +0100

    Fix deployment integration

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 from utils.paystack import initialize_payment, verify_payment
 # from flask_cors import CORS
@@ -45,3 +46,6 @@ if __name__ == '__main__':
     app.run(debug=True)
 else:
     app.run(debug=True)
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
